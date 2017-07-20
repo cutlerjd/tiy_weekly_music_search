5d19ec6afe85f29f6b189c1cc4e976184647f101
Jase Cutler <jasecutler@gmail.com>
2017-07-20 13:52:47 -0700

    Returning things from Apple API

--- starter_files/main.js
+++ starter_files/main.js
@@ -7,3 +7,18 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+let url = new URL ("/search?term=","https://itunes.apple.com")
+console.log(url)
+
+fetch(url + "u2").then(function(response){
+  console.log(response)  
+  return response
+})
+.then(function(response) {
+  return response.json();
+})
+.then(function(musicData) {
+    console.log(musicData)
+    return musicData
+});
